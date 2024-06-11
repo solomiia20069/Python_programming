ddf7e0fa8eea2527351d261747a083c6cefa8df7
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-06-11 23:40:56 +0200

    homework

--- lesson-2.1.py
+++ lesson-2.1.py
@@ -3,7 +3,7 @@ num = input("Enter your five digit number")
 str = num % 10
 reversed_number = reversed_number * 10 + str
 num = num // 10
-
+print(f"Hello {num}")
 reversed_number = 5
 
 
--- lesson-2.py
+++ lesson-2.py
@@ -1,13 +1,13 @@
-num_int = input("Enter a four digit number:")
+num_int = int(input("Enter a four digit number:"))
 
-ones = num_int // 10
-tens = (num_int // 10) % 10)
+ones = (num_int // 1) % 10
+tens = (num_int // 10) % 10
 hundreds = (num_int // 100) % 10
 thousands = (num_int // 1000) % 10
 
-print("ones")
-print("tens")
-print("hundreds")
-print("thousands")
+print(ones)
+print(tens)
+print(hundreds)
+print(thousands)
 
 
