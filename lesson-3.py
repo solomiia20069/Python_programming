251ea5b2c654ae02afc330cc1f6b540750dd6c35
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-06-12 10:12:10 +0200

    homework

---
+++ lesson-3.py
@@ -0,0 +1,17 @@
+number_1 = int(input("Enter your first number"))
+number_2 = int(input("Enter your second number"))
+operation = input("Enter a math operation  -, +, *, /:")
+
+result = 0
+if operation == '+':
+    result = number_1 + number_2
+elif operation == '-':
+    result = number_1 - number_2
+elif operation == '*':
+    result = number_1 * number_2
+elif operation == '/':
+    result = number_1 / number_2
+else:
+    print("Character inputted is not valid")
+
+print(f"{number_1}{operation}{number_2}={result}")
