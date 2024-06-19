8c9f86de33fb6861e2de33c8a855455251ab4f1f
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-06-19 11:06:02 +0200

    homework

---
+++ lesson-3.3.py
@@ -0,0 +1,18 @@
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[0:3], my_list[3:6])
+
+value_list = [1, 2, 3]
+print(value_list[0:2], value_list[2:])
+
+list_1 = [1, 2, 3, 4, 5]
+print(list_1[0:3], list_1[3:5])
+
+list_2 = [1]
+print(list_2[:], list_2[:0])
+
+list_3 = []
+print(list_3[:], list_3[:])
+
+
+#list3 : str[input("enter list:")] ???
+#print(list3.split("")) ???
