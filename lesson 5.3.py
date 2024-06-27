ab7e08ace677a605a0cf2bc62ce6af1ef2bae767
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-06-27 10:31:09 +0200

    homework

--- lesson 5.3.py
+++ lesson 5.3.py
@@ -1,3 +1,5 @@
+import string
+
 value_str = input("Enter the string:")
 new_value = ""
 value_title = value_str.title()
@@ -5,6 +7,11 @@ for symb in value_title:
     if not symb.isspace():
         new_value += symb
 
+new_value = ""
+
+for char in value_str:
+    if char not in string.punctuation:
+        new_value += char
 
 print(("#") + new_value)
 
