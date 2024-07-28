3f567f63f0e4c4a3629e896a8ea3657a6c044504
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-07-28 21:31:08 +0200

    homework

---
+++ lesson 12.2.py
@@ -0,0 +1,57 @@
+class Item:
+    def __init__(self, name, price, description, dimensions):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+
+    def __str__(self):
+        return f"{self.name}, price: {self.price}"
+
+class User:
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        if item in self.products:
+            self.products[item] += cnt
+        else:
+            self.products[item] = cnt
+        self.total = self.get_total()
+
+    def get_total(self):
+        return sum(item.price * cnt for item, cnt in self.products.items())
+
+    def __str__(self):
+        items_str = "\n".join([f"{item.name}: {cnt} pcs." for item, cnt in self.products.items()])
+        return f"User: {self.user}\nItems:\n{items_str}"
+
+lemon = Item('lemon', 5, "yellow", "small")
+apple = Item('apple', 2, "red", "middle")
+
+print(lemon)
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+
+cart.add_item
