fd10da9df7123ae68c3ec0d810c30d23ac5a9e92
solomiiaivanets <ivanetssolomiia11@gmail.com>
2024-08-06 18:50:56 +0200

    homework

---
+++ lesson 15.2.py
@@ -0,0 +1,59 @@
+class Fraction:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            numerator = self.a * other.a
+            denominator = self.b * other.b
+            return Fraction(numerator, denominator)
+        return NotImplemented
+
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            numerator = self.a * other.b + other.a * self.b
+            denominator = self.b * other.b
+            return Fraction(numerator,denominator)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Fraction):
+            numerator = self.a * other.b - other.a * self.b
+            denominator = self.b * other.b
+            return Fraction(numerator, denominator)
+        return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return self.a / self.b == other.a / other.b
+
+    def __gt__(self, other):
+        if isinstance(other, Fraction):
+            return self.a * other.b > other.a * self.b
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Fraction):
+            return self.a * other.b < other.a * self.b
+        return NotImplemented
+
+    def __str__(self):
+        return f"Fraction: {self.a}, {self.b}"
+
+f_a = Fraction(2, 3)
+f_b = Fraction(3, 6)
+f_c = f_b + f_a
+assert str(f_c) == 'Fraction: 21, 18'
+f_d = f_b * f_a
+assert str(f_d) == 'Fraction: 6, 18'
+f_e = f_a - f_b
+assert str(f_e) == 'Fraction: 3, 18'
+
+assert f_d < f_c  # True
+assert f_d > f_e  # True
+assert f_a != f_b  # True
+f_1 = Fraction(2, 4)
+f_2 = Fraction(3, 6)
+assert f_1 == f_2  # True
+print('OK')
